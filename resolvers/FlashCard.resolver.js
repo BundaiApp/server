5d25ff4dde0f16bcb12d5f28cf680d36d9d5767d
kaiyes <kaiyes.ansary@gmail.com>
2023-12-16 20:00:32 +0600

    add cards now respects ifExists

--- resolvers/FlashCard.resolver.js
+++ resolvers/FlashCard.resolver.js
@@ -1,24 +1,28 @@
 import FlashCards from '../models/flashcards.model.js'
 
-const reviewIntervals = { 1: 1, 2: 2, 3: 7, 4: 14, 5: 30, 6: 120 } // Days until next review
-
 export default {
   addFlashCard: async (
     _,
     { userId, kanjiName, hiragana, meanings, quizAnswers }
   ) => {
-    return await FlashCards.create({
-      userId,
-      kanjiName,
-      hiragana,
-      meanings,
-      quizAnswers,
-      rating: 1,
-      firstSeen: new Date(),
-      lastSeen: new Date(),
-      nextReview: new Date()
-      //nextReview: new Date('Dec 25, 2023 01:15:00')
-    })
+    const ifExists = await FlashCards.findOne({ userId, kanjiName })
+
+    if (ifExists) {
+      return
+    } else {
+      return await FlashCards.create({
+        userId,
+        kanjiName,
+        hiragana,
+        meanings,
+        quizAnswers,
+        rating: 1,
+        firstSeen: new Date(),
+        lastSeen: new Date(),
+        nextReview: new Date()
+        //nextReview: new Date('Dec 25, 2023 01:15:00')
+      })
+    }
   },
 
   getPendingFlashCards: async (_, { userId }) => {
@@ -28,3 +32,5 @@ export default {
     })
   }
 }
+
+//const reviewIntervals = { 1: 1, 2: 2, 3: 7, 4: 14, 5: 30, 6: 120 } // Days until next review
